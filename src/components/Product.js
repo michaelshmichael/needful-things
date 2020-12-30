a0246f21947cda35ccdad548d8dc98fb4a884cc8
logeybogey <michaeldcjohnson@gmail.com>
2020-12-30 15:38:37 +0600

    included own button adapted from fomantic UI on product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -22,11 +22,12 @@ export default function Product (props) {
                         <div className='product-image-container'>
                             <img className='product-image' src={product.gallery[0]} alt={product.id}></img>
                         </div>
-                        <div className='product-blurb'>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        <button onClick={props.addItemToBasket}>Add to Basket</button>
+                        <div className='product-blurb-and-button'>
+                            <div className='product-blurb'>
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                            </div>
+                            <button onClick={props.addItemToBasket} className='uibutton'>Add to Basket</button>
                         </div>
-                        
                     </div>
                 </div>
             </motion.div>
